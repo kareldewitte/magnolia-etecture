e14d3fc34beb94ffc6414c55f2cb75b46d5cfbdb
Karel De Witte <karel.dewitte@magnolia-cms.com>
2014-12-15 17:01:16 +0100

    wsdl client generation config and sample promos class

---
+++ landi-relaunch/landi-relaunch-site/src/main/java/de/etecture/magnolia/module/components/Promos.java
@@ -0,0 +1,23 @@
+package de.etecture.magnolia.module.components;
+
+
+import ch.agronet.webservice.Article;
+
+
+/**
+ * Created by kdewitte on 15/12/14.
+ */
+public class Promos {
+
+       private Article article;
+
+
+        public Promos(){
+
+        }
+
+
+
+
+
+}
